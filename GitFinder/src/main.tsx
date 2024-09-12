b9f1800b71188dc3d8c6e39af9f5f444ff98fccc
chillypalmer <ivhighvibesxx@gmail.com>
2024-09-12 09:28:03 -0400

    added port fix

--- GitFinder/src/main.tsx
+++ GitFinder/src/main.tsx
@@ -7,6 +7,8 @@ import CandidateSearch from './pages/CandidateSearch.tsx';
 import SavedCandidates from './pages/SavedCandidates.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
 
+
+
 const router = createBrowserRouter([
   {
     path: '/',
